63ec5feedc59af649e68102debbcd3755139059f
iamishalkin <iamishalkin@edu.hse.ru>
2020-09-07 11:32:59 +0300

    [EN-1930] plot added

--- app.py
+++ app.py
@@ -14,20 +14,60 @@ from dash.dependencies import Input, Output
 
 
 # DxFeed init
-date_time = datetime.now() - relativedelta(hours=1)
+date_time = datetime.now() - relativedelta(days=5)
 endpoint = dx.Endpoint('demo.dxfeed.com:7300')
 
 candle_subscription = endpoint.create_subscription('Candle', date_time=date_time)
-candle_handler = CandleHandler(40)
-ibm_events_to_show = 40
+candle_handler = CandleHandler(100)
 candle_subscription.set_event_handler(candle_handler).add_symbols(['AAPL&Q{=5m}', 'IBM&Q{=5m}'])
 
 # Dash app
 app = dash.Dash(__name__)
 app.layout = html.Div([
-    # html.Link(rel='stylesheet', href='/static/stylesheet.css'),
+    html.Link(rel='stylesheet', href='/static/stylesheet.css'),
     dcc.Markdown(TEXTS.get('header')),
+    html.Div([
+        dcc.Interval(
+                id='interval-component',
+                interval=1*1000,  # in milliseconds
+                n_intervals=0
+            ),
+            dcc.Graph(id='candle-graph'),
+            dcc.RadioItems(
+                id='candle-stocks',
+                options=[
+                    {'label': 'AAPL', 'value': 'AAPL'},
+                    {'label': 'IBM', 'value': 'IBM'},
+                ],
+                value='AAPL'
+            )
+    ])
 ])
+
+
+@app.callback(Output('candle-graph', 'figure'),
+              [Input('interval-component', 'n_intervals'),
+               Input('candle-stocks', 'value')])
+def update_candle_graph(n, stocks):
+    plots = list()  # List with data to display
+    if 'AAPL' in stocks:
+        plots.append(go.Candlestick(x=candle_handler.aapl_data['Time'].safe_get(),
+                                    open=candle_handler.aapl_data['Open'].safe_get(),
+                                    high=candle_handler.aapl_data['High'].safe_get(),
+                                    low=candle_handler.aapl_data['Low'].safe_get(),
+                                    close=candle_handler.aapl_data['Close'].safe_get(),
+                                    name='AAPL'))
+    if 'IBM' in stocks:
+        plots.append(go.Candlestick(x=candle_handler.ibm_data['Time'].safe_get(),
+                                    open=candle_handler.ibm_data['Open'].safe_get(),
+                                    high=candle_handler.ibm_data['High'].safe_get(),
+                                    low=candle_handler.ibm_data['Low'].safe_get(),
+                                    close=candle_handler.ibm_data['Close'].safe_get(),
+                                    name='IBM'))
+
+    return dict(data=plots, layout=go.Layout(title='AAPL & IBM realtime delayed candles',
+                                             showlegend=False,
+                                             uirevision=True))
 
 
 if __name__ == '__main__':
